aa6254539c690aa283185b4746add090895398b2
Ash <136570879+its-meAsh@users.noreply.github.com>
2023-10-20 21:33:32 +0530

    Add files via upload

---
+++ location.js
@@ -0,0 +1,13 @@
+const dc = document.querySelector("#dc");
+const git = document.querySelector("#git");
+const fem = document.querySelector("#fem");
+const spo = document.querySelector("#spo");
+const gdev = document.querySelector("#gdev");
+const web = document.querySelector("#web");
+
+dc.addEventListener('click',()=>{location.replace("https://www.discord.com/app")})
+git.addEventListener('click',()=>{location.replace("https://www.github.com/session")})
+fem.addEventListener('click',()=>{location.replace("https://www.frontendmentor.io/home")})
+spo.addEventListener('click',()=>{location.replace("https://www.open.spotify.com")})
+gdev.addEventListener('click',()=>{location.replace("https://www.developers.google.com/")})
+web.addEventListener('click',()=>{location.replace("https://www.its-meash.github.io/ItsMeAsh/")})
